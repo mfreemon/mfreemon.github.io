84524a43242074abc2e9be46f2844fc0901c833a
Marcus <dfreemon@gmail.com>
2021-01-20 16:24:21 -0800

    updated fonts to be responsive

--- client/src/Components/Work/Work.js
+++ client/src/Components/Work/Work.js
@@ -11,16 +11,16 @@ const Work = () => {
   return (
     <div className="work">
       <div>
-        <h3>I'm a fullstack engineer with more of a frontend bent. I've worked with
+        <p>I'm a fullstack engineer with more of a frontend bent. I've worked with
           and for companies and startups like Idean and Vungle, helped agencies
           like Golin deliver awesome web experiences, and contracted with enterprise
           companies like Autodesk to help deliver new products to market.
-        </h3>
+        </p>
         <br/>
-        <h3>I have 6 year of experiences as developer and really enjoy working
+        <p>I have over 6 years of experience as developer and really enjoy working
           with React but am always exploring new frontend frameworks and expanding
           my knowledge with server-side development.
-        </h3>
+        </p>
       </div>
       <img className="frontend" src={frontend} alt="frontend"/>
       <div className="logos">
